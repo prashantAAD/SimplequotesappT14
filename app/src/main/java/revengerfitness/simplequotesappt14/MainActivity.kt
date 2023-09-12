d2c5f08cb93f6ab07bb86265a8fa5fc27510e3d8
prashant <prashant191000@gmail.com>
2023-09-12 21:29:50 +0530

    Initial commit

---
+++ app/src/main/java/revengerfitness/simplequotesappt14/MainActivity.kt
@@ -0,0 +1,16 @@
+package revengerfitness.simplequotesappt14
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Simple quotes app T14"
+include(":app")
+ 
